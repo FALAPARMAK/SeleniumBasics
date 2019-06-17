a2987ebd6a4a923a92175b3dca716243b843b9f2
FALAPARMAK <betulalaparmak1@gmail.com>
2019-06-17 13:24:24 -0500

    Last code

--- src/com/class02/Amazon.java
+++ src/com/class02/Amazon.java
@@ -1,11 +1,17 @@
 package com.class02;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Amazon {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver","/Users/newowner/Selenium/chromedriver" );
 		WebDriver driver=new ChromeDriver();
@@ -18,7 +24,23 @@ public class Amazon {
 		}else {
 			System.out.println("Sorry!This is not right tittle");
 		}
-
+		
+		
+		
+		TakesScreenshot ts=(TakesScreenshot)driver;
+		File sell=ts.getScreenshotAs(OutputType.FILE);
+		
+		
+		try {
+			FileUtils.copyFile(sell,new File("Users/newowner/Desktop/screen/sell.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("selfie not taken");
+		}
+		Thread.sleep(3000);
+        driver.close();
 	}
+	
 
 }
--- src/com/class08/Action.java
+++ src/com/class08/Action.java
@@ -1,22 +1,45 @@
 package com.class08;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 
 import util.CommonMethods;
 
-public class Action extends CommonMethods{
+public class Action extends CommonMethods {
 
 	public static void main(String[] args) throws InterruptedException {
 		CommonMethods.setUpDriver("chrome", "http://uitestpractice.com/Students/Actions");
 		Actions act=new Actions(driver);
+		
+		
+		//act.click(driver.findElement(By.xpath("//button[@name='click']"))).perform();
+		//Thread.sleep(2000);
+		act.doubleClick(driver.findElement(By.xpath("//button[@name='dblClick']"))).perform();
+		Thread.sleep(2000);
 		WebElement source=driver.findElement(By.id("draggable"));
 		WebElement target=driver.findElement(By.id("droppable"));
 		Thread.sleep(3000);
 		act.dragAndDrop(source, target).build().perform();
 		
-
+//		
+//		System.setProperty("webdriver.chrome.driver","/Users/newowner/Selenium/chromedriver");
+//		WebDriver driver=new ChromeDriver();
+//		driver.get("http://uitestpractice.com");
+//		Actions act=new Actions(driver);
+//		//
+//		
+//		;
+//		
+//		Thread.sleep(3000);
+//		WebElement source=driver.findElement(By.id("draggable"));
+//		WebElement target=driver.findElement(By.id("droppable"));
+//		Thread.sleep(3000);
+//		act.dragAndDrop(source, target).build().perform();
+//		
 	}
-
+	
 }
